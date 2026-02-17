2664665332e85d74cca41065fa6205fbd8cf27ab
sachin yadav <sachin.yadav.sdet@gmail.com>
2026-02-17 18:42:49 +0530

    Funtions concept full details

---
+++ src/OOPsConceptPart1/FunctionsInJava.java
@@ -0,0 +1,58 @@
+package OOPsConceptPart1;
+
+public class FunctionsInJava {
+
+	// main method is --> starting point of execution
+	public static void main(String[] args) {
+		
+		FunctionsInJava obj = new FunctionsInJava();
+		//one object will be created , obj is the reference variable , referring to this object
+		//after creating the object , the copy of all non static methods will be given to this object
+		obj.test();
+		int l = obj.pqr();
+		System.out.println(l);
+		
+		String s1 = obj.qa();
+		System.out.println(s1);
+		
+		int v = obj.division(30, 10);
+		System.out.println(v);
+		
+
+	}
+	// non static method
+	// void -- does not return any value
+	// retrun type -- void
+	public void test() {// no input , no output
+		
+		System.out.println("test method");
+	}
+	
+	// return type --> int
+	public int pqr() { // no input but some output
+		
+		System.out.println("PQR method");
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+		return c;
+	}
+	//return type --> string
+	public String qa() { // no input but some output
+		System.out.println("qa method");
+		String s = "Selenium";
+		return s;
+		
+	}
+	// return type --> int
+	//x,y --> input parameters/arguments
+	public int division(int x, int y) {
+		System.out.println("division method");
+		int d = x/y;
+		
+		return d;
+		
+	}
+	
+
+}
